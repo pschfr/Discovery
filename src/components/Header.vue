3b8c38052373599c6daf0a4e4518a5e9bb1d42c9
Paul Schaefer <yeppaul2@gmail.com>
2019-01-17 11:33:50 -0500

    Deleted Vue logo, renamed HelloWorld component to Header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <h1>{{ msg }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 0;
+}
+</style>
